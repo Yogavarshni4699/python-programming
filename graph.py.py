0df737f62be716ca86252a265b1410b4527968be
Yogavarshni <91062811+Yogavarshni4699@users.noreply.github.com>
2023-09-16 21:29:18 -0700

    Add files via upload

---
+++ graph.py.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+drop=st.selectbox("What type of plot to choose?",options=("BarPlot","No Plot"),index=0,help="Choose a plot option in dropdown",disabled=False)
+st.write("You chose", drop,"Chart")
+
+df1=pd.read_csv("fish.csv")
+df1
+if drop=="BarPlot":
+
+    x=list(df1.iloc[:,0])
+    y=list(df1.iloc[:,1])
+    plt.bar(x,y)
+    plt.xlabel('Species')
+    plt.ylabel('Weight')
+    plt.title('Weight of each Species')
+    
+    BarPlot=plt.show()
+    st.pyplot(BarPlot)
+
+    
+
+
+# In[ ]:
+
+
+
+
