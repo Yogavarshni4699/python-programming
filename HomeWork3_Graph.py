d7f31f08000784d13e13b90a345842ac1e0f9573
Yogavarshni <91062811+Yogavarshni4699@users.noreply.github.com>
2023-09-16 21:01:52 -0700

    Add files via upload

---
+++ HomeWork3_Graph.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+drop=st.selectbox("What type of plot to choose?",options=("ScatterPlot","BarPlot","BoxPlot","LinePlot","HistPlot"),index=0,help="Choose a plot option in dropdown",disabled=False)
+st.write("You chose", drop,"Chart")
+
+df1=pd.read_csv("fish.csv")
+df1
+if drop=="ScatterPlot":
+    st.write("Here is my first attempt to use streamlit")
+    
+    x=list(df1.iloc[:,1])
+    y=list(df1.iloc[:,6])
+    plt.scatter(x,y)
+    plt.xlabel('Weight')
+    plt.ylabel('Width')
+    plt.title('Weight to Width of species')
+    ScatterPlot=plt.show()
+    st.title("Plots Example")
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(ScatterPlot)
+    
+elif drop=="BarPlot":
+
+    x=list(df1.iloc[:,0])
+    y=list(df1.iloc[:,1])
+    plt.bar(x,y)
+    plt.xlabel('Species')
+    plt.ylabel('Weight')
+    plt.title('Weight of each Species')
+    
+    BarPlot=plt.show()
+
