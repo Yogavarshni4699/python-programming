a64a0730642612df10cf88003fa5eed93e32f602
Yogavarshni <91062811+Yogavarshni4699@users.noreply.github.com>
2023-09-16 21:48:01 -0700

    Update graph.py.py

--- graph.py.py
+++ graph.py.py
@@ -8,23 +8,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-drop=st.selectbox("What type of plot to choose?",options=("BarPlot","No Plot"),index=0,help="Choose a plot option in dropdown")
-st.write("You chose", drop,"Chart")
-
 df1=pd.read_csv("Fish.csv")
 df1
-if drop=="BarPlot":
 
-    x=list(df1.iloc[:,0])
-    y=list(df1.iloc[:,1])
-    plt.bar(x,y)
-    plt.xlabel('Species')
-    plt.ylabel('Weight')
-    plt.title('Weight of each Species')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+x=list(df1.iloc[:,0])
+y=list(df1.iloc[:,1])
+plt.bar(x,y)
+plt.xlabel('Species')
+plt.ylabel('Weight')
+plt.title('Weight of each Species')
+st.set_option('deprecation.showPyplotGlobalUse', False)
     
-    BarPlot=plt.show()
-    st.pyplot(BarPlot)
+BarPlot=plt.show()
+st.pyplot(BarPlot)
 
     
 
