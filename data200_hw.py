24efaec19b32362ee358ed0ac2e2336c6ecb06cd
Yogavarshni <91062811+Yogavarshni4699@users.noreply.github.com>
2023-09-28 12:04:00 -0700

    Update data200_hw.py

--- data200_hw.py
+++ data200_hw.py
@@ -16,20 +16,38 @@ Churn_df.duplicated().sum()
 #Missing Values/ Null Values
 Churn_df.isnull().sum()
 
+#Visualizing null value
+# Display the heatmap
+st.write("Heatmap to check Missing Data:")
 fig, ax = plt.subplots()
 sns.heatmap(Churn_df.isnull(), ax=ax)
 st.write(fig)
 
-#Visualizing null value
-# Display the heatmap
-#st.write("Heatmap to check Missing Data:")
-#plot = sns.heatmap(Churn_df.isnull(), annot=True) 
-# Display the plot in Streamlit
-#st.pyplot(plot.get_figure())
-
 st.write("to know about the dataset")
-Churn_df.columns
+#Churn_df.columns
 Churn_df1=Churn_df.describe()
 st.dataframe(Churn_df1)
 
+#checking on the unique value
+for i in Churn_df:
+  print("No of unique value in",i,"is",Churn_df[i].nunique())
+  
+st.write("Few findings from the data:")
+
+Churned=Churn_df[Churn_df['Churn']== True].Churn.count()
+st.write("No of customers churning:", Churned)
+
+CustChurn_df= Churn_df[Churn_df['Churn']== True]
+
+#unique areacode
+areacode=Churn_df['Area code'].nunique()
+st.write("No of unique area code",areacode)
+
+#International Plan
+International=Churn_df[Churn_df['International plan']=="Yes"]['International plan'].count()
+st.write("No of customers having International plan", International)
+
+#Voice mail plan
+Voice_mail=Churn_df[Churn_df['Voice mail plan']=="Yes"]['Voice mail plan'].count()
+st.write("No of customers having Voice mail plan", Voice_mail)
 
